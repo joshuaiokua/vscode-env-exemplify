fca748194fd76b8214c7d12592fc725dbc28ba39
joshuaiokua <jiokua.albano@gmail.com>
2024-07-10 01:50:42 -0700

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,26 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	console.log('Extension "vscode-env-exemplify" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-env-exemplify" is now active!');
+    let disposable = vscode.workspace.onDidSaveTextDocument((document) => {
+        // Check if the saved file is an .env file
+        if (document.fileName.endsWith('.env')) {
+            const filePath = document.fileName;
+            const copyPath = filePath + '.example';
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('vscode-env-exemplify.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from env-exemplify!');
-	});
-
-	context.subscriptions.push(disposable);
+            // Copy the .env file to .env.example
+            fs.copyFile(filePath, copyPath, (err) => {
+                if (err) {
+                    vscode.window.showErrorMessage(`Failed to create a copy of ${filePath}: ${err.message}`);
+                } else {
+                    vscode.window.showInformationMessage(`Copied ${filePath} to ${copyPath}`);
+                }
+            });
+        }
+    });
 }
 
 // This method is called when your extension is deactivated
